10cd2782950ecbf027fb94745f4847b898e2826c
dvberkel <daan.v.berkel.1980@gmail.com>
2015-09-22 20:28:27 +0200

    Save code after recording

--- calculations/src/main.rs
+++ calculations/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("The answer: {}", 42);
+    println!("What is 6 times 7: {}", 6 * 7);
+    println!("What is 17 plus 25: {}", 17 + 25);
+    println!("What is 17 plus 5 times 5: {}", 17 + 5 * 5);
 }
